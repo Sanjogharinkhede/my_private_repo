2c9cffd9a6bce590b8a4192967c426b3ade3f190
sanjog harinkhede <sanjogharinkhede@gmail.com>
2025-01-20 12:30:50 +0530

    ADDED:Assignment till SDLC completed.

---
+++ mysql/books.sql
@@ -0,0 +1,8 @@
+INSERT INTO Books (Title, Author, ISBN, Publisher, PublishedYear, TotalCopies, AvailableCopies, Genre)
+VALUES 
+('Pride and Prejudice', 'Jane Austen', '9780141439518', 'Modern Library', 1993, 10, 10, 'Classic'),
+('Moby Dick', 'Herman Melville', '9781503280786', 'Penguin Classics', 1951, 8, 8, 'Adventure'),
+('Great' 'Expectations','Charles Dickens',9780141439563,'Penguin Classics',1961,10,10,'Classic'),
+('War and Peace','Leo Tolstoy',9781400079988,'Vintage',1969,12,12,'Historical Fiction'),
+('Anna Karenina','Leo Tolstoy',9780143035008,'Penguin Classics',1977,10,9,'Classic'),
+('Wuthering Heights','Emily Brontë',9780141439556,'Penguin Classics',1947,8,8,'Classic');
